718416b9bf358b2018b9a71bc0a1882a63d07c1a
ulag-india <ulag.india@gmail.com>
2018-04-22 00:04:59 +0530

    Code has been updated for param annotations

--- src/main/java/com/mypackage/rest/MyResource.java
+++ src/main/java/com/mypackage/rest/MyResource.java
@@ -2,16 +2,21 @@ package com.mypackage.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
+@Path("{pathParam}/test")
 public class MyResource {
+	
+	@PathParam("pathParam") private String pathParamExample;
+	@QueryParam("query") private String queryParamExample;
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod()
 	{
-		return "It works!";
+		return "It works! Path Param used " + pathParamExample + " and Query Param used " + queryParamExample;
 	}
 }
